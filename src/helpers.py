b80fc977713de272bdbb62b85463580a3c930813
John Ayling <johnayling@yahoo.com>
2025-08-27 06:51:44 -0500

    Add extract_markdown_images helper and enhance split_nodes_delimiter logic with additional test cases.

--- src/helpers.py
+++ src/helpers.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 """
 Splits a list of TextNodes into a list of TextNodes, where the delimiter is used to split the text of the TextNodes.
@@ -11,10 +12,31 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_nodes.append(node)
         else:
             sections = node.text.split(delimiter)
-            if (len(sections) != 3):
+            if (len(sections) % 2 == 0):
                 raise ValueError("Invalid text node")
-            new_nodes.append(TextNode(sections[0], TextType.TEXT))
-            new_nodes.append(TextNode(sections[1], text_type))
-            new_nodes.append(TextNode(sections[2], TextType.TEXT))
+            
+            is_text = True
+            for section in sections:
+                if (is_text):
+                    new_nodes.append(TextNode(section, TextType.TEXT))
+                else:
+                    new_nodes.append(TextNode(section, text_type))
+                is_text = not is_text
 
+            
     return new_nodes
+
+"""
+Extracts all the images from a text string.
+"""
+def extract_markdown_images(text):
+    images = []
+    for image in re.findall(r"!\[(.*?)\]\((.*?)\)", text):
+        images.append(image)
+    return images
+
+def split_nodes_image(old_nodes):
+    pass
+
+def split_nodes_link(old_nodes):
+    pass
--- src/test_helpers.py
+++ src/test_helpers.py
@@ -1,10 +1,10 @@
 import unittest
 
-from helpers import split_nodes_delimiter
+from helpers import split_nodes_delimiter, extract_markdown_images, split_nodes_image, split_nodes_link
 from textnode import TextNode, TextType
 
 class TestHelpers(unittest.TestCase):
-    def test_split_nodes_delimiter_code(self):
+    def test_split_nodes_delimiter_code_one(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
         self.assertEqual(len(new_nodes), 3)
@@ -14,6 +14,51 @@ class TestHelpers(unittest.TestCase):
         self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
         self.assertEqual(new_nodes[1].text_type, TextType.CODE)
         self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+
+    def test_split_nodes_delimiter_code_multiple(self):
+        node = TextNode("This is text with a `code block` word and `code block 2` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(len(new_nodes), 5)
+        self.assertEqual(new_nodes[0].text, "This is text with a ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "code block")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[2].text, " word and ")
+        self.assertEqual(new_nodes[3].text, "code block 2")
+        self.assertEqual(new_nodes[3].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[4].text, " word")
+        self.assertEqual(new_nodes[4].text_type, TextType.TEXT)
+
+    def test_split_nodes_delimiter_code_multiple_side_by_side(self):
+        node = TextNode("This is text with a `code block``code block 2` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(len(new_nodes), 5)
+        self.assertEqual(new_nodes[0].text, "This is text with a ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "code block")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[2].text, "")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[3].text, "code block 2")
+        self.assertEqual(new_nodes[3].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[4].text, " word")
+        self.assertEqual(new_nodes[4].text_type, TextType.TEXT)
+
+
+    ''' 
+        should throw an error
+    '''
+    def test_split_nodes_delimiter_code_open(self):
+        node = TextNode("This is text with a `code block", TextType.TEXT)
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter([node], "`", TextType.CODE)
+
+    def test_split_nodes_delimiter_code_none(self):
+        node = TextNode("This is text with a word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(len(new_nodes), 1)
+        self.assertEqual(new_nodes[0].text, "This is text with a word")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
 
     def test_split_nodes_delimiter_bold(self):
         node = TextNode("This is **bold**", TextType.TEXT)
@@ -37,3 +82,31 @@ class TestHelpers(unittest.TestCase):
         self.assertEqual(new_nodes[1].text_type, TextType.ITALIC)
         self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
 
+    def test_extract_markdown_images_one(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_images_two(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and ![image2](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png"), ("image2", "https://i.imgur.com/fJRm4Vk.jpeg")], matches)
+
+    # def test_split_nodes_image(self):
+    #     node = TextNode(
+    #         "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+    #         TextType.TEXT,
+    #     )
+    #     new_nodes = split_nodes_link([node])
+
+    #     self.assertEqual(len(new_nodes), 4)
+    #     self.assertEqual(new_nodes[0].text, "This is text with a link ")
+    #     self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+    #     self.assertEqual(new_nodes[1].text, "to boot dev")
+    #     self.assertEqual(new_nodes[1].text_type, TextType.LINK)
+    #     self.assertEqual(new_nodes[2].text, " and ")
+    #     self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+    #     self.assertEqual(new_nodes[3].text, "to youtube")
+    #     self.assertEqual(new_nodes[3].text_type, TextType.LINK)
